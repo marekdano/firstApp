d5cb19178bd435b9a4c6b4cc28e330f7bea59c6a
Marek Dano <mk.dano@gmail.com>
2016-01-07 21:58:10 +0000

    Added carousel in home page which is dynamically populated from database. Added partial 'product' in the product view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @random_total_products = Random.new.rand(2..Product.count)
+    @products = Product.limit(@random_total_products)
+    #@products = Product.limit(3)
   end
 end
