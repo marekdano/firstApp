30d9331fc85faf3f72ad7976fc9df25e70c76605
marekdano <mk.dano@gmail.com>
2016-01-19 17:14:10 +0000

    Added the configuration, business logic and views for handling to send emails using the SendGrid add-on on heroku.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,9 +15,12 @@ class HomeController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    ActionMailer::Base.mail(:from => @email, 
-      :to => 'mk.dano@gmail.com',
-      :subject => "A new message contact form message from #{@name}",
-      :body => @message).deliver_now
+    
+    #ActionMailer::Base.mail(:from => @email, 
+    #  :to => 'mk.dano@gmail.com',
+    #  :subject => "A new message contact form message from #{@name}",
+    #  :body => @message).deliver_now
+    
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 end
