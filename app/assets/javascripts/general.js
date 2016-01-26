4048dc98cf98e2aca9144562f40b765a69b12440
marekdano <mk.dano@gmail.com>
2016-01-26 15:37:43 +0000

    Added 'raty' jquery plugin to the product show view and the commnets.

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,4 +1,15 @@
 
+$(document).on('ready page:load', function(){
+  $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+});
+
 $(function() {
   $(".alert").delay(4000).fadeOut(1500);
 });
+
