42207e46ab4ad050b97797583da082f1da4a11e9
marekdano <mk.dano@gmail.com>
2016-01-29 12:31:14 +0000

    Added validation to User, Comment and Product models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :orders
+
+  validatable :first_name, :last_name, presence: true
 end
