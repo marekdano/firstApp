a3311dbdefd7527f09c2fc05b2544a57937cee5c
Marek Dano <mk.dano@gmail.com>
2016-04-08 00:43:13 +0100

    Fixed the failing test cases in the comment controller spec.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 describe CommentsController, :type => :controller do 
   before do 
     @user = FactoryGirl.create(:user)
-    @admin = FactoryGirl.build(:admin)
+    @admin = FactoryGirl.create(:admin)
     @product = FactoryGirl.create(:product)
-    @product.comments.create!(rating: 4, user: @user, body: "Awful bike!")
+    #@product.comments.create!(rating: 4, user: @user, body: "Awful bike!")
   end
 
   describe "Post #create" do 
     context "User is NOT logged in," do
       
       it "create a new comment" do 
-        @product.comments.create!(rating: 4, user: @user, body: "Awful bike!")
-        expect(response).to be_success
-        expect(response).to have_http_status(200)
+        comment_params = FactoryGirl.attributes_for(:comment, user: @user)
+        expect { post :create, comment: comment_params, product_id: @product.id }
+        .to change(Comment, :count).by(0)
       end
     end
 
@@ -24,12 +24,9 @@ describe CommentsController, :type => :controller do
       end
 
       it "create another new comment" do 
-        # this test case failing
-        comment_params = FactoryGirl.attributes_for(:comment)
-        expect { post :create, :comment => comment_params }
+        comment_params = FactoryGirl.attributes_for(:comment, user: @user)
+        expect { post :create, comment: comment_params, product_id: @product.id }
         .to change(Comment, :count).by(1)
-
-        expect(response).to be_success
       end
     end
   end
@@ -40,9 +37,11 @@ describe CommentsController, :type => :controller do
         sign_in @admin
       end
 
-      it "delete the comment" do
-        delete :destroy, id: @product.comments.id
-        expect(response).to be_success
+      it "delete the comment" do 
+        comment = FactoryGirl.create(:comment, product_id: @product.id)
+        # Also, test if the action really deletes a comment.
+        expect { delete :destroy, id: comment.id, product_id: @product.id}
+        .to change{ @product.comments.count }.by(-1)
       end
     end
   end
