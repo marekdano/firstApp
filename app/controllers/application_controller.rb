64bcd66e552bf39e3ff7b12b7b381a46c3e489bc
Marek Dano <mk.dano@gmail.com>
2016-04-08 22:07:15 +0100

    Add the feature of redirection to the current page after user login and logout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,28 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
   end
+
+  before_filter :store_action
+
+  def store_action
+    return unless request.get? 
+    if (request.path != "/users/login" &&
+        request.path != "/users/logout" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_up" &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:user, request.fullpath)
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer || root_path
+  end
+
 end
---
+++ spec/features/redirection_in_login_logout_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe "display products page" do
+  before do
+    create_products
+    @user = FactoryGirl.create(:user)
+  end
+
+  it "user logs in" do
+    visit "/products"
+    visit "/users/login"
+    within("#new_user") do
+      fill_in 'user_email', :with => @user.email
+      fill_in 'user_password', :with => @user.password
+    end
+    click_button "Log in"
+    expect(page).to have_current_path("/products")
+  end
+
+  it "user logs out" do
+    visit "/products"
+    visit "/users/login"
+    within("#new_user") do
+      fill_in 'user_email', :with => @user.email
+      fill_in 'user_password', :with => @user.password
+    end
+    click_button "Log in"
+    visit "/products/1"
+    click_link "Logout"
+    expect(page).to have_current_path("/products/1")
+  end
+end
