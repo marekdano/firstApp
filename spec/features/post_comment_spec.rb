c57b9bf58eca2d64675e21d07a255ff8d525bfba
Marek Dano <mk.dano@gmail.com>
2016-04-11 10:37:36 +0100

    Add test cases for a new comment.

--- spec/features/post_comment_spec.rb
+++ spec/features/post_comment_spec.rb
@@ -3,17 +3,39 @@ require "rails_helper"
 describe "submit a message" do
   before do
     create_products
+    login_user
+    Capybara.current_driver = :selenium
+    visit "/products/bike"
   end
   
-  it "comment body is empty" do
-    login_user
+  it "when comment body is empty" do
     visit "/products/bike"
     within("#new-comment-box") do
       fill_in 'comment_body', :with => ""
-      fill_in 'rating', :with => ""
     end
-    click_button 'Submit'
+    find(:xpath, "//img[@title='poor']").click
+    click_button "Submit"
 
     expect(page).to have_content "Comment cannot be blank and rating should be selected."
   end
+
+  it "when rating is not selected" do
+    within("#new-comment-box") do
+      fill_in 'comment_body', :with => "nice bike!!!"
+    end
+    click_button "Submit"
+
+    expect(page).to have_content "Comment cannot be blank and rating should be selected."
+  end
+
+ it "when comment is filled and rating is selected" do
+  within("#new-comment-box") do
+    fill_in 'comment_body', :with => "nice bike!!!"
+  end
+  find(:xpath, "//img[@title='poor']").click
+  click_button "Submit"
+
+  expect(page).to have_content "Thank you for your review!"
+end
+
 end
