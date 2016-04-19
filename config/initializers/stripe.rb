92d801fc73bd4c627de580e89abe931d3344d774
Marek Dano <mk.dano@gmail.com>
2016-04-19 18:39:02 +0100

    Configured the stripe test environment on the rails production.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,10 @@
 
 if Rails.env.production?
   Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-    :secret_key => ENV['STRIPE_SECRET_KEY']
+    #:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    #:secret_key => ENV['STRIPE_SECRET_KEY']
+    :publishable_key => 'pk_test_DAAbQhPBQgC8ivtmlB74vdE7',
+    :secret_key => 'sk_test_56ZiqgZPEetWddXR1346wSYB'
   }
 else
   Rails.configuration.stripe = {
