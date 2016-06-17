482828f14a00353d231a46581071b65f78b9fe4f
Marek Dano <mk.dano@gmail.com>
2016-06-17 14:14:37 +0100

    Add logging for a built user.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     # The method below is implemented the model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    puts @user
     
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
