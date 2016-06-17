8f329bdedcf646daa86571934e32fb7a23468b4e
Marek Dano <mk.dano@gmail.com>
2016-06-17 14:04:02 +0100

    Add logging for omniauth data received from facebook.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    puts request.env["omniauth.auth"]
+
+    # The method below is implemented the model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    #byebug
+    
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
